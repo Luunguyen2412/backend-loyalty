916f498bb9260a31dca24bad8e6803eebd19d3ef
Viết Lưu Nguyễn <71163248+Luunguyen2412@users.noreply.github.com>
2023-06-26 22:30:20 +0700

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getContacts, getContact, createContact, updateContact, deleteContact } = require("../controllers/contactController");
 const validateToken = require("../middleware/validateTokenHandle");
 
-router.use(validateToken);
+// router.use(validateToken);   // set condition: must have token when call api
 
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
