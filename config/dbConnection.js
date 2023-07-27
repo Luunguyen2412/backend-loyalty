0f4a4de2b9c2043bcdc952f6c60e482f7bd41d5d
Viết Lưu Nguyễn <71163248+Luunguyen2412@users.noreply.github.com>
2023-07-27 21:41:13 +0700

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    // const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(
+      "mongodb+srv://admin:admin@dipeshcluster.ssctvog.mongodb.net/my-loyalty-backend?retryWrites=true&w=majority"
+    );
+
     console.log(
       "Database connected: ",
       connect.connection.host,
