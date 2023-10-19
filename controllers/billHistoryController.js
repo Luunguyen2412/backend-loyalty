73181e8ce31d922fc9a6d67fd20e29efa606c28b
Viết Lưu Nguyễn <71163248+Luunguyen2412@users.noreply.github.com>
2023-10-19 22:34:15 +0700

    update getBillHistoryForUser, updatePointsForUser

--- controllers/billHistoryController.js
+++ controllers/billHistoryController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const BillHistory = require("../models/billHistoryModel");
+const User = require("../models/userModel");
+
 // Get all Bill History
 // GET /api/bills
 const getBills = asyncHandler(async (req, res) => {
@@ -7,6 +9,21 @@ const getBills = asyncHandler(async (req, res) => {
   const bills = await BillHistory.find();
   res.status(200).json(bills);
 });
+
+// Function to fetch bill history for a specific user
+const getBillHistoryForUser = async (userId) => {
+  try {
+    // Use Mongoose to query the BillHistory collection
+    const billHistory = await BillHistory.find({
+      user: "64b1124f579b88d18dc244e1",
+    }).exec();
+    return billHistory;
+  } catch (error) {
+    // Handle any errors that occur during the query
+    console.error("Error fetching bill history:", error);
+    throw error;
+  }
+};
 
 // Create new bill
 // POST /api/bills
@@ -39,7 +56,40 @@ const createBill = asyncHandler(async (req, res) => {
   console.log(`create bill successfull `);
 });
 
+// Update User Point when Payment succesfully
+// POST /api/updatePoints/:userId
+const updatePointsForUser = async (userId) => {
+  try {
+    // Get the user and their associated bill histories
+    const user = await User.findById(userId);
+    const billHistories = await BillHistory.find({ user: userId });
+
+    // Calculate total points earned based on the bill histories
+    let totalPoints = 0;
+    for (const billHistory of billHistories) {
+      totalPoints += calculatePointsFromBillHistory(billHistory);
+    }
+
+    // Update the user's points
+    user.point = totalPoints;
+    await user.save();
+
+    return user;
+  } catch (error) {
+    console.error("Error updating points:", error);
+    throw error;
+  }
+};
+
+const calculatePointsFromBillHistory = (billHistory) => {
+  // Customize this function to calculate points based on totalBill
+  const pointsPerDollar = 0.001; // 1 point for every 10000đ
+  return billHistory.totalBill * pointsPerDollar;
+};
+
 module.exports = {
   getBills,
   createBill,
+  updatePointsForUser,
+  getBillHistoryForUser,
 };
--- routes/billHistoryRoutes.js
+++ routes/billHistoryRoutes.js
@@ -2,10 +2,36 @@ const express = require("express");
 const {
   getBills,
   createBill,
+  updatePointsForUser,
+  getBillHistoryForUser,
 } = require("../controllers/billHistoryController");
 
 const router = express.Router();
 
 router.route("/").get(getBills).post(createBill);
+// router.route("/:userId").get(getBillHistoryForUser);
+// router.route("/updatePoints/:userId").post(updatePointsForUser);
+
+router.get("/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const billHistory = await getBillHistoryForUser(userId);
+    res.json(billHistory);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch bill history" });
+  }
+});
+
+router.post("/updatePoints/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const user = await updatePointsForUser(userId);
+    res.json({ message: "Points updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update points" });
+  }
+});
 
 module.exports = router;
